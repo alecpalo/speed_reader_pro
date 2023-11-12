8aa01dfb386003d25a21c696f4ded364e11b8ce2
Ramiz Sohail <rsohail@umich.edu>
2023-11-12 15:17:16 -0500

    testing

--- lib/screen/UploadFile.dart
+++ lib/screen/UploadFile.dart
@@ -1,80 +1,80 @@
-// // creating uploading file capabilities
-// import 'dart:io';
-// import 'package:file_picker/file_picker.dart';
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter/material.dart';
+// creating uploading file capabilities
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class UploadFile extends StatefulWidget {
+  const UploadFile({super.key});
+
+  @override
+  State<UploadFile> createState() => _UploadFileState();
+}
+
+class _UploadFileState extends State<UploadFile> {
+  File? file;
+  FilePickerResult? result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('File Picker')),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          if (file != null || result != null) ...[
+            if (kIsWeb) ...[
+              Image.memory(
+                result!.files.first.bytes!,
+                height: 350,
+                width: 350,
+                fit: BoxFit.fill,
+              ),
+            ] else ...[
+              Image.file(file!, height: 150, width: 150, fit: BoxFit.fill),
+            ],
+            const SizedBox(height: 8),
+          ],
+          ElevatedButton(
+            onPressed: () async {
+              try {
+                result = await FilePicker.platform.pickFiles();
+                if (result != null) {
+                  if (!kIsWeb) {
+                    file = File(result!.files.single.path!);
+                  }
+                  setState(() {});
+                } else {
+                  // User canceled the picker
+                }
+              } catch (_) {}
+            },
+            child: const Text('Pick File'),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
-// class UploadFile extends StatefulWidget {
-//   const UploadFile({super.key});
+// FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true);
 //
-//   @override
-//   State<UploadFile> createState() => _UploadFileState();
+// if (result != null) {
+//   List<File> files = result.paths.map((path) => File(path)).toList();
+// } else {
+//   // User canceled the picker
 // }
 //
-// class _UploadFileState extends State<UploadFile> {
-//   File? file;
-//   FilePickerResult? result;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('File Picker')),
-//       body: Center(
-//         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-//           if (file != null || result != null) ...[
-//             if (kIsWeb) ...[
-//               Image.memory(
-//                 result!.files.first.bytes!,
-//                 height: 350,
-//                 width: 350,
-//                 fit: BoxFit.fill,
-//               ),
-//             ] else ...[
-//               Image.file(file!, height: 150, width: 150, fit: BoxFit.fill),
-//             ],
-//             const SizedBox(height: 8),
-//           ],
-//           ElevatedButton(
-//             onPressed: () async {
-//               try {
-//                 result = await FilePicker.platform.pickFiles();
-//                 if (result != null) {
-//                   if (!kIsWeb) {
-//                     file = File(result!.files.single.path!);
-//                   }
-//                   setState(() {});
-//                 } else {
-//                   // User canceled the picker
-//                 }
-//               } catch (_) {}
-//             },
-//             child: const Text('Pick File'),
-//           ),
-//         ]),
-//       ),
-//     );
-//   }
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// //
-// // FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true);
-// //
-// // if (result != null) {
-// //   List<File> files = result.paths.map((path) => File(path)).toList();
-// // } else {
-// //   // User canceled the picker
-// // }
-// //
